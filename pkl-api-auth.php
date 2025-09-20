bd5da5cf7dd18bc9b1d4397e05c0b360ca0d3bca
Palmiiz Kittinan <104422927+PalmiizKittinan@users.noreply.github.com>
2025-09-20 09:40:35 +0700

    Update pkl-api-auth.php

--- pkl-api-auth.php
+++ pkl-api-auth.php
@@ -3,7 +3,7 @@
  * Plugin Name: PKL REST API Auth
  * Plugin URI: https://github.com/PalmiizKittinan/pkl-rest-api-auth
  * Description: Control WordPress REST API access by requiring user authentication with API key system.
- * Version: -test.0.8
+ * Version: 2.2.0
  * Author: Kittinan Lamkaek
  * Author URI: https://github.com/PalmiizKittinan
  * License: GPL v2 or later
@@ -259,4 +259,5 @@ function pkl_rest_api_auth() {
 
 // Initialize plugin
 pkl_rest_api_auth();
+
 
